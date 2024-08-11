732eef420f5acdeb0fa8ad433ec042bde4326549
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-11 17:05:33 +0530

    Update EdgeBrowserTigger.java

--- EdgeBrowserTigger.java
+++ EdgeBrowserTigger.java
@@ -8,14 +8,12 @@ public class Test1 {
        WebDriver driver1 = new EdgeDriver();
        driver1.get("https://www.google.com");
       driver1.get("https://www.github.com/Karthick2174/Java_Program");
-       driver1.manage().timeouts().implicitlyWait(8500, TimeUnit.SECONDS);
        driver1.manage().window().maximize();
        driver1.manage().window().minimize();
        driver1.manage().window().maximize();
        driver1.getTitle();
        System.out.println(driver1.getTitle());
        System.out.println(driver1.getCurrentUrl());
-       Thread.sleep(500);
       driver1.quit();
     }
 }
