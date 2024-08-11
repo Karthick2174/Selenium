c715f2aa8fad6de23867608c8223eb032f280687
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-11 16:01:09 +0530

    Create EdgeBrowserTigger.java

---
+++ EdgeBrowserTigger.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Test1 {
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws InterruptedException {
+       WebDriver driver1 = new EdgeDriver();
+       driver1.get("https://www.google.com");
+      driver1.get("https://www.github.com/Karthick2174/Java_Program");
+       driver1.manage().timeouts().implicitlyWait(8500, TimeUnit.SECONDS);
+       driver1.manage().window().maximize();
+       driver1.manage().window().minimize();
+       driver1.manage().window().maximize();
+       driver1.getTitle();
+       System.out.println(driver1.getTitle());
+       System.out.println(driver1.getCurrentUrl());
+       Thread.sleep(500);
+      driver1.quit();
+    }
+}
