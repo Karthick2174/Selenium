19ef9d223cda480b3122bccc51ef0e4ab1feb948
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-13 01:06:04 +0530

    Update SearchInGoogle.java

--- SearchInGoogle.java
+++ SearchInGoogle.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selftest2{
    public static void main(String[] args) {
       WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
       driver.get("https://www.google.com/");
       WebElement p=driver.findElement(By.name("q"));
       p.sendKeys("Selenium Java");
