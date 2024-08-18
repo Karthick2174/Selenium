a2187e84d85b8a7d002347a597849bc0fb61f91e
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-18 22:25:58 +0530

    Create SpanTextCompare.java

---
+++ SpanTextCompare.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class One {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("Https://google.com");
+		WebElement p = driver.findElement(By.name("q"));
+		p.sendKeys("Selenium Key");
+		p.submit();
+        WebElement spanElement = driver.findElement(By.className("VuuXrf"));
+        String actualSpanText = spanElement.getText();
+        String expectedSpanText = "Selenium";
+        if (actualSpanText.equals(expectedSpanText)) {
+            System.out.println("Text matches: " + actualSpanText);
+        } else {
+            System.out.println("Text does not match. Found: " + actualSpanText);
+        }
+        driver.quit();
+    }
+}
