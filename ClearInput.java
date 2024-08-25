fec3e20d6352c7ad862374b8a6d1cdd62b852211
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-25 19:09:03 +0530

    Create ClearInput.java

---
+++ ClearInput.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class One {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("Https://google.com");
+		WebElement p = driver.findElement(By.name("q"));
+		p.sendKeys("Selenium Key");
+		Thread.sleep(2000);
+		p.clear();
+		Thread.sleep(2000);
+		p.sendKeys("Selenium Key");
+		p.submit();
+        WebElement spanElement = driver.findElement(By.className("VuuXrf"));
+        spanElement.click();
+        String actualSpanText = spanElement.getText();
+        String expectedSpanText = "Selenium";
+        if (actualSpanText.equals(expectedSpanText)) {
+            System.out.println("Text matches: " + actualSpanText);
+        } else {
+            System.out.println("Text does not match. Found: " + actualSpanText);
+        }
+      driver.quit();
+    }
+}
