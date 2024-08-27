c140126f74c78d13e852a7d7eb9a7a8720f2c781
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2024-08-27 22:17:11 +0530

    Update SearchInGoogle.java

--- SearchInGoogle.java
+++ SearchInGoogle.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Selftest2{
+public class SearchInGoogle{
    public static void main(String[] args) {
       WebDriver driver = new ChromeDriver();
       driver.manage().window().maximize();
