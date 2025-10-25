b8bc62e3f75929ec6bc5c0e32abcadf0f27eed74
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2025-10-26 02:35:34 +0530

    Create selectDatePicker.java

---
+++ selectDatePicker.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class selectDatePicker {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver Drive = new ChromeDriver();
+        try {
+            Drive.get("https://jqueryui.com/datepicker/");
+            Thread.sleep(2000); // Wait for page load
+            Drive.manage().window().maximize();
+            Thread.sleep(2000);
+            Drive.switchTo().frame(0); 
+            Drive.findElement(By.xpath("//input[@id='datepicker']")).sendKeys("10/01/2025");
+            System.out.println("Date filled successfully!!");
+        } catch (Exception e) {
+            System.err.println("Test Failed! Error: " + e.getMessage());
+            
+        } finally {
+            Thread.sleep(3000);
+            if (Drive != null) {
+                Drive.quit(); 
+            }
+        }
+    }
+}
