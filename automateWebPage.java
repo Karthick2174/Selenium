fa92044739f5a9e555d4894180f7b13a3d908f79
Karthikeyan <94568769+Karthick2174@users.noreply.github.com>
2026-03-02 22:23:15 +0530

    Clean up imports in automateWebPage.java
    Removed unused import statement for WebElement.

--- automateWebPage.java
+++ automateWebPage.java
@@ -2,11 +2,9 @@ import org.testng.Assert;
 import java.util.List;
 import org.openqa.selenium.WebElement;
 import java.time.Duration;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
